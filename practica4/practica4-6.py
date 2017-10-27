0b40250a781e82a5b269785263e731721fa87013
kancha13 <32647074+kancha13@users.noreply.github.com>
2017-10-27 18:18:32 +0200

    Add files via upload

---
+++ practica4/practica4-6.py
@@ -0,0 +1,7 @@
+
+altura=input("dame altura")
+
+for i in range(altura):
+    for j in range(i+1):
+        print"*",
+    print ""
---
+++ practica4/practica4-7.py
@@ -0,0 +1,6 @@
+altura=input("dame altura")
+
+for i in range(altura):
+    for j in range(altura-i,0,(-1)):
+        print"*",
+    print ""
